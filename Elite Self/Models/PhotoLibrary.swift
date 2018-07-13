165e58966ce8a2bcbb0ea44f8ad1936fe5473261
Alexandra <alexandra.beznosova@gmail.com>
2018-07-13 21:43:32 +0300

    Small updates
    got rid of some unnecessary lines of comments and code.

--- Elite Self/Models/PhotoLibrary.swift
+++ Elite Self/Models/PhotoLibrary.swift
@@ -11,7 +11,7 @@ import os.log
 
 class PhotoLibrary: NSObject, NSCoding {
     var image: UIImage?
-    //var caption: String // left it commented in case i will need it in the future updates
+    //var caption: String // I left it commented in case i will need it in the future updates
     static var DocumentsDictionary = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
     static let ArchiveURL = DocumentsDictionary.appendingPathComponent("photoLibrary")
     // MARK: - Types
@@ -31,14 +31,14 @@ class PhotoLibrary: NSObject, NSCoding {
         //aCoder.encode(caption, forKey: PropertyKey.caption)
     }
     required convenience init?(coder aDecoder: NSCoder) {
-        // Because photo is an optional property of Meal, just use conditional cast.
+        
         let image = aDecoder.decodeObject(forKey: PropertyKey.image) as? UIImage
-        // The name is required. If we cannot decode a name string, the initializer should fail.
+        
         /*guard let caption = aDecoder.decodeObject(forKey: PropertyKey.caption) as? String else {
          os_log("Unable to decode the caption for a Photo caption.", log: OSLog.default, type: .debug)
          return nil
          }*/
-        // Must call designated initializer.
+
         self.init(image: image)//, caption: caption)
     }
 }
