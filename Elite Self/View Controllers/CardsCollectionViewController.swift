ec646249f58e811c83b1e654fe48936cb2d22610
Alexandra <alexandra.beznosova@gmail.com>
2018-10-01 22:35:40 +0300

    added new files
    added new View Controllers Scene files

---
+++ Elite Self/View Controllers/CardsCollectionViewController.swift
@@ -0,0 +1,48 @@
+//
+//  CardsCollectionViewController.swift
+//  Elite Self
+//
+//  Created by Alexandra Beznosova on 01/10/2018.
+//  Copyright © 2018 Divine App. All rights reserved.
+//
+
+import UIKit
+
+class CardsCollectionViewController: UICollectionViewController, StoryboardedVCs {
+    
+    weak var coordinator: MainCoordinator?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+    // MARK: - Navigation bar
+
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cardsCell", for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+
+}
